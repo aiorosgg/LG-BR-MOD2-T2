2c25e6d275601e71a843d778b67c5e78a6efb1eb
Lucas Glaydson <gameslukinhas5@hotmail.com>
2022-09-27 22:19:35 -0300

    LG - Dino - LARGE_CACTUS

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,19 +1,20 @@
 import pygame
 
 from dino_runner.components.obstacles.cactus import Cactus
-from dino_runner.utils.constants import SMALL_CACTUS
+from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS
 
 
 class ObstacleManager:
+    
     def __init__(self):
         self.obstacles = []
 
     def update(self, game):
         if len(self.obstacles) == 0:
-            self.obstacles.append(Cactus(SMALL_CACTUS))
-
+            self.obstacles.append(Cactus(SMALL_CACTUS + LARGE_CACTUS))
+            
         for obstacle in self.obstacles:
-            obstacle.update(game.game_speed, self.obstacles)
+            obstacle.update(game.game_speed, self.obstacles)           
             if game.player.dino_rect.colliderect(obstacle.rect):
                 pygame.time.delay(500)
                 game.playing = False
